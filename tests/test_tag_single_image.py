ffc473df962878d10e767815a0ad2353ece7470d
tiboitel <jules.boitelle@gmail.com>
2025-05-21 15:51:10 +0200

    fix(tests): Resolves a path issue in unit tests following the transition of Tagster to a module that prevented tests from running correctly.
    - Changed path to src/tagster/cli.py
    - Added the subprocess import to tests/test_tag_single_ima.ge.py

--- tests/test_tag_single_image.py
+++ tests/test_tag_single_image.py
@@ -1,5 +1,8 @@
+# tests/test_tag_single_image.py
+import subprocess
+
 def test_tag_single_image():
     img = "tests/assets/test.jpg"
-    result = subprocess.run(["python", "src/cli.py", img], capture_output=True, text=True)
+    result = subprocess.run(["python", "src/tagster/cli.py", img], capture_output=True, text=True)
     assert "→" in result.stdout
 
